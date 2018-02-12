96e636eb628f3e9dc43ef86ab79034b720a0277c
Emil <emil.ringgaard@hotmail.com>
2018-02-12 19:36:38 +0100

    added some test travis stuff

--- USV-autopilot/Sample-Test1/test.cpp
+++ USV-autopilot/Sample-Test1/test.cpp
@@ -10,7 +10,7 @@ TEST(TestCaseName, TestName) {
 
 
 TEST(SalutationTest, Static) {
-	EXPECT_EQ(string("Hello World!"), Salutation::greet("Worldd"));
+	EXPECT_EQ(string("Hello World!"), Salutation::greet("World"));
 }
 
 int main(int argc, char* argv[])
