333a05ab0aa8331e5fbff6a169056025c977224f
Emil <emil.ringgaard@hotmail.com>
2018-02-12 18:00:47 +0100

    added some test travis stuff

---
+++ USV-autopilot/Sample-Test1/Source.h
@@ -0,0 +1,15 @@
+#include "pch.h"
+#include <ostream>
+#include <iostream>
+using namespace std;
+
+class Salutation
+{
+public:
+	static string greet(const string& name);
+};
+
+///////////////////////////////////////
+// In the class implementation file
+
+
--- USV-autopilot/Sample-Test1/test.cpp
+++ USV-autopilot/Sample-Test1/test.cpp
@@ -1,12 +1,19 @@
 #include "pch.h"
+#include "Source.h"
+#include <iostream>
 
+using namespace std;
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
 }
 
+string Salutation::greet(const string& name) {
+	ostringstream s;
+	s << "Hello " << name << "!";
+	return s.str();
+}
 
-int main()
-{
-	
+TEST(SalutationTest, Static) {
+	EXPECT_EQ(string("Hello World!"), Salutation::greet("World"));
 }
